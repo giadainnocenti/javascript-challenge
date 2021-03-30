2c81ce98137ebae288f3a3f6e54ee8866317259c
giadainnocenti <giadainnocenti@users.noreply.github.com>
2021-03-30 17:26:51 -0400

    started to develop the code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,26 @@
 // from data.js
 var tableData = data;
+//select the button
+var button = d3.select("#button");
+//select the form
+var form = d3.select("#form");
 
+//create event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+//creating the function to handle the events
+function runEnter() {
+    //preventing the page from refreshing
+    d3.event.preventDefault();
+    // select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+    //get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputvalue);
+    console.log(data);
+
+
+}
 // YOUR CODE HERE!
